742bf1e78def397d472cd2511dc506244daa7935
ander94ds <ander94ds@gmail.com>
2022-03-24 13:36:02 -0400

    [ADD] smart button full logic

--- ve_tax_withholding/models/account_move.py
+++ ve_tax_withholding/models/account_move.py
@@ -4,3 +4,14 @@ class AccountMove( models.Model):
     _inherit = 'account.move'
     control_number = fields.Char(string='Numero de control', required=True, default='00')
     
+    withholdings_counter = fields.Integer(string='counter' compute='_compute_withholding_counter') 
+    
+    @api.model
+    def _compute_withholding_counter(self):
+        for record in self:
+            count = self.env['tax.withholding_voucher_vendor'].search_count([('related_invoice', '=', record.id)])
+            record.withholdings_counter = count
+            
+    @api.model
+    def open_model_test(self):
+        print('Prueba')
